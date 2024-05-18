3595871099adcfe224f39f17e1ab1435a0e07171
Alpha Mokurumi <mokurumialpha@gmail.com>
2024-05-18 06:23:27 +0300

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 '''
-Redis exercise
+web cache tracker
 '''
 
 
@@ -10,32 +10,13 @@ from typing import Callable
 from functools import wraps
 
 
-redis_store = redis.Redis()
-
-
-def data_cacher(method: Callable) -> Callable:
-    '''
-    data_cacher decorator
-    '''
-    @wraps(method)
-    def invoker(url) -> str:
-        '''
-        invoker method
-        '''
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
-        if result:
-            return result.decode('utf-8')
-        result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
-        return result
-    return invoker
-
-
-@data_cacher
 def get_page(url: str) -> str:
     '''
-    get_page method
+    get_page function
     '''
+    r = redis.Redis()
+    key = f"count:{url}"
+    r.incr(key)
+    r.expire(key, 10)
+
     return requests.get(url).text
